09ed653d81f32c85577d88703919adbb4d118ae0
metallikus <metalarchus@gmail.com>
2018-08-23 17:19:13 +0400

    Add all files

---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+    #echo "АЗАЗАЗАЗА";
+});
+
+Route::get('page', function () {
+    return view('page');
+});
+
+Route::get('azaza', function () {
+    echo '<pre>';
+    //print_r($_ENV);
+    echo Config::set('app.locale', 'ru');
+    echo Config::get('app.locale');
+    echo '</pre>';
+});
+
+
+//Route::post('azaza', function () {
+//    echo '<pre>';
+//    //print_r($_ENV);
+//    echo Config::set('app.locale', 'ru');
+//    echo Config::get('app.locale');
+//    echo '</pre>';
+//});
